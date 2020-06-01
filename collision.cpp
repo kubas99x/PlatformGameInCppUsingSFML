44806f651ef0744244e4ee5c46e1970dace31373
kubas99x <kubas99x@users.noreply.github.com>
2020-06-01 13:31:19 +0200

    teraz chyba lepiej

--- collision.cpp
+++ collision.cpp
@@ -23,9 +23,6 @@ bool collision::check_standing_collision(const sf::Sprite &hero ,  const float &
     float guy_position_x = guy.getPosition ().x + guy_width;
     float guy_position_y = guy.getPosition ().y + guy_height + next_move;
 
-    //juz dla wszystkich platform
-//    std::cout<<"#############################"<<std::endl;
-//    std::cout<<guy_position_y<<"   "<<guy_height<<std::endl;
 
     for(auto &el : this->vector_collision_platforms)
     {
@@ -39,8 +36,6 @@ bool collision::check_standing_collision(const sf::Sprite &hero ,  const float &
 
             float intersect_x = std::abs(delta_x) - (guy_width + el_width) ;
             float intersect_y = std::abs(delta_y) - (guy_height + el_height);
-//            std::cout<< el_position_y<<"    "<<el_height<<std::endl;
-//            std::cout<<"Intersect x:  "<<intersect_x<<"  intersect y:  "<<intersect_y <<std::endl;
 
             if(intersect_x < 0.0f && intersect_y < 0.0f )
             {
@@ -60,7 +55,6 @@ bool collision::check_walking_collision(const sf::Sprite &hero , const float &ne
     float guy_position_x = guy.getPosition ().x + guy_width  + next_move;
     float guy_position_y = guy.getPosition ().y + guy_height - 1 ;      // -1 bo czasami wartosc intersect_y jest na poziomie ujemnych czesci tysiecznych
 
-//    std::cout<<"##########################################"<<std::endl;
     for(auto &el : this->vector_collision_platforms)
     {
             float el_width = el.getGlobalBounds ().width/2.0f;
@@ -73,8 +67,6 @@ bool collision::check_walking_collision(const sf::Sprite &hero , const float &ne
 
             float intersect_x = std::abs(delta_x) - (guy_width + el_width);
             float intersect_y = std::abs(delta_y) - (guy_height + el_height);
-//            std::cout<<"El width:  "<<el_width<<"  position  "<<el_position_x <<std::endl;
-//            std::cout<<"Intersect x:  "<<intersect_x<<"  intersect y:  "<<intersect_y <<std::endl;
 
             if(intersect_x < 0.0f && intersect_y < 0.0f )
             {
